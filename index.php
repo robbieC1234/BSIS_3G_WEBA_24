1c4e6859b4a5d843fbf8df678ab70c61d4634ce5
robbieC1234 <robbieclerigo13@gmail.com>
2024-01-24 11:22:12 -0800

    update index.php

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<!DOCTYPE html> 
-<
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GitHubBSIS3G</title>
+</head>
+<body>
+    
+</body>
+</html>
